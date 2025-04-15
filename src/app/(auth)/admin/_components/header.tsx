d8a1bd69eaa08e40dfb228cf50a59002005db2ca
baloerrr <akbaradityah444@gmail.com>
2025-04-16 00:00:33 +0700

    add: header admin with title props

---
+++ src/app/(auth)/admin/_components/header.tsx
@@ -0,0 +1,15 @@
+export function Header({ title }: { title: string }) {
+  return (
+    <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6">
+      <h2 className="text-3xl font-bold">{title}</h2>
+      <div className="text-sm text-muted-foreground">
+        {new Date().toLocaleDateString("id-ID", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </div>
+    </div>
+  );
+}
