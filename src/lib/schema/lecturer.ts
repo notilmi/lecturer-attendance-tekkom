fc6742e69e3a112c4f6201d725db556ed93c707c
baloerrr <akbaradityah444@gmail.com>
2025-04-17 17:57:18 +0700

    update: schema lecturer

--- src/lib/schema/lecturer.ts
+++ src/lib/schema/lecturer.ts
@@ -1,10 +1,12 @@
-import { z } from "zod";
+import { string, z } from "zod";
 
 export const lecturerSchema = z.object({
     name: z.string().min(1, "Nama wajib diisi"),
     lecturerCode: z.string().min(3, "Kode dosen minimal 3 karakter"),
     rfidUid: z.string().min(1, "RfidUid wajib diisi"),
-    status: z.string().min(1, "Status wajib diisi")
+    status: z.string().min(1, "Status wajib diisi"),
+    lastUpdated: z.number().min(1),
+    teachingDays: z.string().min(1)
 });
 
 export type Lecturer = z.infer<typeof lecturerSchema>
