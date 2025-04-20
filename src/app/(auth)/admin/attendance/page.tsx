ffc3c0c9aa20b501bb10cc8ef6e529a6957fcf31
baloerrr <akbaradityah444@gmail.com>
2025-04-21 01:36:32 +0700

    feat: root file for attendance

---
+++ src/app/(auth)/admin/attendance/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import { Tabs } from "@/components/ui/tabs";
+import { Header } from "../_components/header";
+import { UseAttendancePage } from "./use-attendance-page";
+import { TabList } from "./_components/tab-list";
+import { DailyReportTab } from "./_components/daily-report-tab";
+import { SummaryTab } from "./_components/summary-tab";
+
+export default function AttendanceReportPage() {
+  const {
+    activeTab,
+    setActiveTab,
+  } = UseAttendancePage();
+
+  return (
+    <div className="p-4">
+      <Header title="Laporan Kehadiran" />
+
+      <Tabs
+        defaultValue="daily"
+        value={activeTab}
+        onValueChange={setActiveTab}
+        className="space-y-6"
+      >
+        <TabList />
+
+        {/* Daily Report Tab */}
+        <DailyReportTab />
+
+        {/* Summary Tab */}
+        <SummaryTab />
+      </Tabs>
+    </div>
+  );
+}
