f4b16c3e6cc0e22447f361991a57bf1756d7b3c1
baloerrr <akbaradityah444@gmail.com>
2025-04-21 01:36:50 +0700

    feat: add header

--- src/app/(auth)/admin/_components/header.tsx
+++ src/app/(auth)/admin/_components/header.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export function Header({ title }: { title: string }) {
   return (
     <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6">
@@ -10,6 +12,15 @@ export function Header({ title }: { title: string }) {
           day: "numeric",
         })}
       </div>
+      {/* <Button
+        onClick={async () => {
+          const res = await fetch("/api/reset-status");
+          const data = await res.json();
+          alert(`Reset status: ${data.message}`);
+        }}
+      >
+        Trigger Reset Status
+      </Button> */}
     </div>
   );
 }
