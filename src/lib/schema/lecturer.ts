5e1281f53a1488586891a562fa96f8a18fcffa84
baloerrr <akbaradityah444@gmail.com>
2025-05-02 17:23:22 +0700

    chore: add id

--- src/lib/schema/lecturer.ts
+++ src/lib/schema/lecturer.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const lecturerSchema = z.object({
+    id: z.string().optional(),
     name: z.string().min(1, "Nama wajib diisi"),
     lecturerCode: z.string().min(3, "Kode dosen minimal 3 karakter"),
     rfidUid: z.string().min(1, "RfidUid wajib diisi"),
