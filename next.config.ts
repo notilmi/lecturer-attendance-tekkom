73b253a8268de62939785266d18bbe28ed477113
baloerrr <akbaradityah444@gmail.com>
2025-07-10 10:40:26 +0700

    feat(next-config): add domain for image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  images: {
+    domains: ['teknikkomputer.polsri.ac.id'],
+  }
 };
 
 export default nextConfig;
