6ce4d8fbf872f59bdc5e4938e126c03cfac2db4c
baloerrr <akbaradityah444@gmail.com>
2025-07-10 10:39:58 +0700

    fix(login): change into 2 column

--- src/app/login/_components/login-form.tsx
+++ src/app/login/_components/login-form.tsx
@@ -17,9 +17,9 @@ import { toast } from "sonner";
 import { z } from "zod";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { AlertCircle } from "lucide-react";
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 import { useAuth } from "@/contexts/auth-context";
-
+import Image from "next/image";
 
 const loginSchema = z.object({
   email: z.string().email("Email tidak valid"),
@@ -33,14 +33,13 @@ export default function LoginForm() {
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
-  const {user} = useAuth()
-
+  const { user } = useAuth();
 
   useEffect(() => {
     if (!loading && user) {
-        router.push("/admin/dashboard");
-      }
-  }, [user, loading, router])
+      router.push("/admin/dashboard");
+    }
+  }, [user, loading, router]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -58,7 +57,7 @@ export default function LoginForm() {
       setLoading(true);
       await signInWithEmailAndPassword(auth, email, password);
       toast.success("Login berhasil");
-      router.push('/admin/dashboard');
+      router.push("/admin/dashboard");
     } catch (error: any) {
       console.log(error);
       setError(error.message);
@@ -68,51 +67,73 @@ export default function LoginForm() {
     }
   };
 
-
-  
-
   return (
-    <Card className="w-full max-w-md">
-      <CardHeader>
-        <CardTitle className="text-2xl font-bold">Login Admin</CardTitle>
-        <CardDescription>
-          Masukkan email dan password untuk masuk ke panel admin sistem absensi
-          dosen
-        </CardDescription>
-      </CardHeader>
+    <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen w-full">
+      <div className="flex items-center justify-center p-6 bg-white">
+        <Card className="w-full max-w-md">
+          <CardHeader className="flex flex-col items-center gap-2">
+            <Image
+              src="/logo_tekkom.png" // bisa public/logo.png
+              alt="Logo Teknik Komputer"
+              width={80}
+              height={80}
+              className="mb-2"
+            />
+            <CardTitle className="text-2xl font-bold text-center">
+              Login Admin
+            </CardTitle>
+            <CardDescription className="text-center">
+              Masukkan email dan password untuk masuk ke panel admin sistem
+              absensi dosen
+            </CardDescription>
+          </CardHeader>
 
-      <CardContent>
-        {error && (
-          <Alert className="max-w-sm mx-auto mb-2" variant="destructive">
-            <AlertCircle className="h-4 w-4" />
-            {/* <AlertTitle>Error</AlertTitle> */}
-            <AlertDescription>{error}</AlertDescription>
-          </Alert>
-        )}
-        <form onSubmit={handleSubmit} className="space-y-4 max-w-sm mx-auto">
-          <Input
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            type="email"
-          />
-          <Input
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-          />
-          <Button className="w-full" type="submit" disabled={loading}>
-            {loading ? <>Logging in...</> : "Login"}
-          </Button>
-        </form>
-      </CardContent>
+          <CardContent>
+            {error && (
+              <Alert className="max-w-sm mx-auto mb-2" variant="destructive">
+                <AlertCircle className="h-4 w-4" />
+                {/* <AlertTitle>Error</AlertTitle> */}
+                <AlertDescription>{error}</AlertDescription>
+              </Alert>
+            )}
+            <form
+              onSubmit={handleSubmit}
+              className="space-y-4 max-w-sm mx-auto"
+            >
+              <Input
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+              />
+              <Input
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+              />
+              <Button className="w-full" type="submit" disabled={loading}>
+                {loading ? <>Logging in...</> : "Login"}
+              </Button>
+            </form>
+          </CardContent>
 
-      <CardFooter className="flex flex-col">
-        <p className="mt-2 text-center text-sm text-muted-foreground">
-          Informasi Kehadiran Dosen © 2025
-        </p>
-      </CardFooter>
-    </Card>
+          <CardFooter className="flex flex-col">
+            <p className="mt-2 text-center text-sm text-muted-foreground">
+              Informasi Kehadiran Dosen © 2025
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="relative hidden md:block w-full h-full min-h-screen">
+        <Image
+          src="https://teknikkomputer.polsri.ac.id/wp-content/uploads/2023/02/BG_FIRST_FEATURED.png"
+          alt="Ilustrasi Login"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+    </div>
   );
 }
